90daf58c30b8abb13428166a44942e900c5cb46b
kimhaechang1 <81299056+kimhaechang1@users.noreply.github.com>
2022-05-20 23:43:53 +0900

    20220520

---
+++ 에라토스테네스의체.py
@@ -0,0 +1,17 @@
+def make_isPrime(n):
+    isPrime = [True]*(n+1)
+    isPrime[0] = False
+    isPrime[1] = False
+    for i in range(2, int((n+1)**(1/2))):
+        if isPrime[i]:
+            for j in range(i+i, n+1, i):
+                isPrime[j] = False
+    return isPrime
+
+def isPrime(n):
+    isPrime = make_isPrime(n)
+    if isPrime[n]:
+        answer = "소수입니다."
+    else:
+        answer = "소수가 아닙니다."
+    return answer
