1136db9f5e2de978ec2531a5c3b5597983cbc5f0
kimhaechang1 <81299056+kimhaechang1@users.noreply.github.com>
2022-05-22 23:18:21 +0900

    20220522

--- 에라토스테네스의체.py
+++ 에라토스테네스의체.py
@@ -2,7 +2,7 @@ def make_isPrime(n):
     isPrime = [True]*(n+1)
     isPrime[0] = False
     isPrime[1] = False
-    for i in range(2, int((n+1)**(1/2))):
+    for i in range(2, int((n+1)**(1/2))+1):
         if isPrime[i]:
             for j in range(i+i, n+1, i):
                 isPrime[j] = False
