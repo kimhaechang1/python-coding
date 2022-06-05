82357b41ccc4dd54e549601dee92016304c28d0f
kimhaechang1 <81299056+kimhaechang1@users.noreply.github.com>
2022-06-05 23:55:32 +0900

    20220527

--- 백준2108.py
+++ 백준2108.py
@@ -90,6 +90,43 @@ nums = [0]*4001 사용
 최빈 값은 계수정렬
 범위는 min, max
 """
+import sys
+import collections
+
+def avg(l,n):
+    print(round(sum(l)/n))
+
+def mid(l,n):
+    l = sorted(l)
+    print(l[n//2])
+
+def fre(l,n):
+    ld = collections.Counter(l)
+    ld = sorted(ld.items(), key = lambda x : x[1], reverse=True)
+    m = ld[0][1]
+    temp = []
+    for key, value in ld :
+        if m == value:
+            temp.append(key)
+    temp = sorted(temp)
+    if len(temp)>1:
+        print(temp[1])
+    else:
+        print(temp[0])
+
+def bound(l):
+    print(max(l)-min(l))
+
+
+n = int(sys.stdin.readline())
+l = []
+for i in range(n):
+    l.append(int(sys.stdin.readline()))
+
+avg(l,n)
+mid(l,n)
+fre(l,n)
+bound(l)
 
 
 
